d1dee80c4d13b10e439a914ecfc6784f1bf0f146
jiawku <jiawku@hotmail.com>
2017-10-04 01:14:25 -0500

    update

--- src/popup.js
+++ src/popup.js
@@ -1,7 +1,19 @@
 function search(){
   var word=document.getElementById('query').value;
   if (word!=""){
-  window.open('https://www.google.com/search?q=Dictionary#dobs='+word, '_blank', 'toolbar=yes, location=yes, status=yes, menubar=yes, scrollbars=yes');
+  // window.open('https://www.google.com/search?q=Dictionary#dobs='+word, '_blank', 'toolbar=yes, location=yes, status=yes, menubar=yes, scrollbars=yes');
+  // window.open('https://www.merriam-webster.com/dictionary/'+word, '_blank', 'toolbar=yes, location=yes, status=yes, menubar=yes, scrollbars=yes');
+  // window.open('http://www.iciba.com/'+word, '_blank', 'toolbar=yes, location=yes, status=yes, menubar=yes, scrollbars=yes');
+
+  chrome.tabs.create({
+    url: 'https://www.google.com/search?q=Dictionary#dobs='+word
+  });
+  chrome.tabs.create({
+    url: 'https://www.merriam-webster.com/dictionary/'+word
+  });
+  chrome.tabs.create({
+    url: 'http://www.iciba.com/'+word
+  });
 }
 };
 
